6a022eccb305bdaa700f59a995ec1f35477e4ef7
andy <andy@FDOS-ABRAYNT-1.local>
2019-02-07 15:48:19 -0500

    cleanup urls

--- backend/urls.py
+++ backend/urls.py
@@ -6,11 +6,3 @@ router.register('api/posts', PostViewSet, 'posts')
 router.register('api/comments', CommentViewSet, 'comments')
 
 urlpatterns = router.urls
-
-# # Wire up our API using automatic URL routing.
-# # Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     path('', include(router.urls)),
-#     path('api-auth/', include('backend.urls', namespace='backend'))
-#     # path('api-auth/', include('backend.urls', namespace='rest_framework'))
-# ]
